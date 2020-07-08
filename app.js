22cbd91d6005ea0776f80f65a87eb48618e7cf46
durban24k <durban24k@users.noreply.github.com>
2020-07-08 17:35:44 +0300

    EJS templating engine

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 let express = require ("express");
 let app = express();
 
-app.get("/",(req,res)=>{
-    res.send("<h1>This is a ToDoList App</h1>");
-});
+app.set("view engine","ejs");
 
+app.get("/",(req,res)=>{
+    res.render("todo.ejs");
+});
+app.get("*",(req,res)=>{
+    res.send("<h1>ERROR 404: PAGE NOT FOUND</h1>")
+});
 app.listen(3000,()=>{
     console.log("Server listening on port: 3000...");
 });

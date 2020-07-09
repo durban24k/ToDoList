1731f71dd52495d3b7652671e2be58eddbeefe93
durban24k <durban24k@users.noreply.github.com>
2020-07-09 10:17:59 +0300

    Ejs addition

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 let express = require ("express");
 let app = express();
+let bodyParser = require("body-parser");
 
 app.set("view engine","ejs");
 
+let toDO=[
+    "Wash the dishes",
+    "clean the house"
+]
 app.get("/",(req,res)=>{
     res.render("todo.ejs");
+});
+app.post("/todo",(req,res)=>{
+
 });
 app.get("*",(req,res)=>{
     res.send("<h1>ERROR 404: PAGE NOT FOUND</h1>")

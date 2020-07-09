3869274ca1dcf8afa06719a0322867238c960345
durban24k <durban24k@users.noreply.github.com>
2020-07-09 11:21:14 +0300

    body-parser

--- app.js
+++ app.js
@@ -3,19 +3,17 @@ let app = express();
 let bodyParser = require("body-parser");
 
 app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
 
-let toDO=[
-    "Wash the dishes",
-    "clean the house"
-]
+let todo = [];
 app.get("/",(req,res)=>{
-    res.render("todo.ejs");
+    res.render("todo.ejs",{todo :todo});
 });
 app.post("/todo",(req,res)=>{
-
-});
-app.get("*",(req,res)=>{
-    res.send("<h1>ERROR 404: PAGE NOT FOUND</h1>")
+    let list = req.body.newlist;
+    todo.push(list);
+    console.log("Item added to list!!");
+    res.redirect("/");
 });
 app.listen(3000,()=>{
     console.log("Server listening on port: 3000...");
